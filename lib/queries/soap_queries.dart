88e0bdef9384e20bcf7b1bdf9c08263d347087f5
moseskamira <moses.african@gmail.com>
2025-03-17 12:58:50 +0300

    Convert to Celcius

--- lib/queries/soap_queries.dart
+++ lib/queries/soap_queries.dart
@@ -15,17 +15,15 @@ class SoapQueries {
   }
 
   static String fahrenheitToCelsius(String fahrenheit) {
-    return '''
-    <?xml version="1.0" encoding="utf-8"?>
-    <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                   xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-                   xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+    return '''<?xml version="1.0" encoding="utf-8"?>
+  <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"
+                 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                 xmlns:xsd="http://www.w3.org/2001/XMLSchema">
       <soap:Body>
-        <FahrenheitToCelsius xmlns="https://www.w3schools.com/xml/">
-          <Fahrenheit>$fahrenheit</Fahrenheit>
-        </FahrenheitToCelsius>
+          <FahrenheitToCelsius xmlns="https://www.w3schools.com/xml/">
+              <Fahrenheit>$fahrenheit</Fahrenheit>
+          </FahrenheitToCelsius>
       </soap:Body>
-    </soap:Envelope>
-    ''';
+  </soap:Envelope>''';
   }
 }
